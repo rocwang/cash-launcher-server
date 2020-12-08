314cce18d06c3a5210d865c4410440bdf3f27ff3
Roc Wang <roc@kiwiberry.nz>
2020-12-08 22:25:35 +1300

    feat: use separate websocket to handle velocity and orientation streams

--- index.ts
+++ index.ts
@@ -1,32 +1,52 @@
+import url from "url";
+import fs from "fs";
+import https from "https";
 import WebSocket from "ws";
-
-const fs = require("fs");
-const https = require("https");
-const ws = require("ws");
 
 const server = https.createServer({
   key: fs.readFileSync("./key.pem"),
   cert: fs.readFileSync("./crt.pem"),
 });
-const wss = new ws.Server({ server });
 
-let currentWebSocket: WebSocket | null = null;
-wss.on("connection", (ws: WebSocket) => {
-  console.log("A new client is connected");
+const wssArray = ["/velocity", "/orientation"].map((path) => {
+  const wss = new WebSocket.Server({ noServer: true });
+  let currentSocket: WebSocket | null = null;
 
-  if (currentWebSocket) {
-    currentWebSocket.close(1000, "Another client is connected");
+  wss.on("connection", (ws: WebSocket) => {
+    console.log("A new client is connected");
+
+    if (currentSocket) {
+      currentSocket.close(1000, "Another client is connected");
+    }
+
+    currentSocket = ws;
+
+    currentSocket.on("message", (message: string) => {
+      console.log(message);
+    });
+
+    currentSocket.on("close", (code: number, reason: string) => {
+      console.log(code, reason);
+    });
+  });
+
+  return {
+    path,
+    wss,
+  };
+});
+
+server.on("upgrade", function upgrade(request, socket, head) {
+  const pathname = url.parse(request.url).pathname;
+
+  const match = wssArray.find(({ path }) => path === pathname);
+  if (match) {
+    match.wss.handleUpgrade(request, socket, head, (ws) => {
+      match.wss.emit("connection", ws, request);
+    });
+  } else {
+    socket.destroy();
   }
-
-  currentWebSocket = ws;
-
-  currentWebSocket.on("message", (message: string) => {
-    console.log(message);
-  });
-
-  currentWebSocket.on("close", (code: number, reason: string) => {
-    console.log(code, reason);
-  });
 });
 
 const port = 443;
